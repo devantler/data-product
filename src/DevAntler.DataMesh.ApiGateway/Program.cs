9ba10c0fe028a106da9e22fd204adf565939d133
nidam16 <nidam16@student.sdu.dk>
2022-09-25 22:55:18 +0200

    Setup Microservice Architecture with Ocelot

--- src/DevAntler.DataMesh.ApiGateway/Program.cs
+++ src/DevAntler.DataMesh.ApiGateway/Program.cs
@@ -6,8 +6,13 @@ builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-await app.UseOcelot();
+app.UseSwagger();
+app.UseSwaggerForOcelotUI(opt => opt.PathToSwaggerGenerator = "/swagger/docs")
+    .UseOcelot().Wait();
 
 app.Run();
--- src/Services/DevAntler.DataMesh.ProvisioningService/Program.cs
+++ src/Services/DevAntler.DataMesh.ProvisioningService/Program.cs
@@ -1,25 +1,18 @@
 using DevAntler.DataMesh.ProvisioningService.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddProvisioningTargets();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
