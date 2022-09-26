4c1dd16cc31fb2342e1557b045528d9dfd14e3e4
nidam16 <nidam16@student.sdu.dk>
2022-09-26 16:21:49 +0200

    Updated launch configurations and added shared SwaggerGenOptionsExtensions

--- src/Services/DevAntler.DataMesh.DataProductService/Program.cs
+++ src/Services/DevAntler.DataMesh.DataProductService/Program.cs
@@ -1,11 +1,13 @@
+using System.Reflection;
 using DevAntler.DataMesh.DataProductService.Extensions;
+using DevAntler.DataMesh.SwaggerGenExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.AddSwaggerDoc();
+    options.AddSwaggerDoc("Data Product API", "v1", "A Web API for managing Data Products");
     options.IncludeXmlComments();
 });
 builder.Services.AddProvisioningTargets();
--- src/Services/DevAntler.DataMesh.SwaggerGenExtensions/SwaggerGenOptionsExtensions.cs
+++ src/Services/DevAntler.DataMesh.SwaggerGenExtensions/SwaggerGenOptionsExtensions.cs
@@ -1,18 +1,20 @@
 using System.Reflection;
+using DevAntler.DataMesh.SwaggerGenExtensions;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
-namespace DevAntler.DataMesh.DataProductService.Extensions;
+namespace DevAntler.DataMesh.SwaggerGenExtensions;
 
 public static class SwaggerGenOptionsExtensions
 {
-    public static void AddSwaggerDoc(this SwaggerGenOptions options)
+    public static void AddSwaggerDoc(this SwaggerGenOptions options, string title, string version, string description)
     {
-        options.SwaggerDoc("v1", new OpenApiInfo
+        options.SwaggerDoc(version, new OpenApiInfo
         {
-            Version = "v1",
-            Title = "Data Product API",
-            Description = "An ASP.NET Core Web API for managing Data Products",
+            Version = version,
+            Title = title,
+            Description = description,
             Contact = new OpenApiContact
             {
                 Name = "Nikolai Emil Damm",
@@ -28,7 +30,7 @@ public static class SwaggerGenOptionsExtensions
 
     public static void IncludeXmlComments(this SwaggerGenOptions options)
     {
-        var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlFileName = $"{Assembly.GetCallingAssembly().GetName().Name}.xml";
         var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFileName);
         options.IncludeXmlComments(xmlPath);
     }
