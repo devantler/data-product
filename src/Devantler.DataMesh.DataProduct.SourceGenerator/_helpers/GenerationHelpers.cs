6a4d8f6f3c784672ff64b1f8677bbd5be67e102e
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-11-25 01:39:22 +0100

    WIP GraphQL

---
+++ src/Devantler.DataMesh.DataProduct.SourceGenerator/_helpers/GenerationHelpers.cs
@@ -0,0 +1,6 @@
+namespace Devantler.DataMesh.DataProduct.SourceGenerator._helpers;
+
+public static class GenerationHelpers
+{
+    internal static string IndentBy(int spaces) => new(' ', spaces);
+}
