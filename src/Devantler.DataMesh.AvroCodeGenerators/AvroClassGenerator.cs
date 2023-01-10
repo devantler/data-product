0cceef0c834d7383fd55a3194d325a4cf4e205fa
Nikolai Emil Damm <nidam16@student.sdu.dk>
2023-01-11 00:47:26 +0100

    Fixed incremental generator errors (#36)
    * Fixed ugly error with Incremental Generators not supporting having members outside of the method scopes.
    
    * Begun writing the custom AvroCodeGenerator
    
    * Added custom AvroCodeGenerator to make some okay looking files.
    
    * Avro Code Generators
    
    * Moved assets to shared classlib, and fixed alot of tests
    
    * Fixed remaining build errors

---
+++ src/Devantler.DataMesh.AvroCodeGenerators/AvroClassGenerator.cs
@@ -0,0 +1,40 @@
+using Devantler.Commons.StringHelpers;
+
+namespace Devantler.DataMesh.AvroCodeGenerators;
+
+/// <summary>
+/// An Avro Code Generator that can generate a C# class from a <see cref="RecordSchema"/>.
+/// </summary>
+public class AvroClassGenerator : IAvroCodeGenerator<RecordSchema>
+{
+    /// <summary>
+    /// Generates code from a <see cref="Schema" />.
+    /// </summary>
+    /// <param name="namespace"></param>
+    /// <param name="schema"></param>
+    /// <returns>The generated code as raw text.</returns>
+    public string Generate(string @namespace, RecordSchema schema)
+    {
+        CodeCompileUnit compileUnit = new();
+        _ = compileUnit.AddNamespace(@namespace);
+        _ = compileUnit.AddType(schema.Name, false, schema.Documentation);
+
+        if (!schema.Fields.Any(x => x.Name.Equals("id", StringComparison.OrdinalIgnoreCase)))
+            _ = compileUnit.AddPropertyWithBackingField("Id", new CodeTypeReference(typeof(Guid)), "The unique identifier of the model.");
+
+        for (int i = 0; i < schema.Fields.Count; i++)
+        {
+            Type type = AvroTypeResolver.ResolveType(schema.Fields[i].Schema.Name);
+            CodeTypeReference codeTypeReference = type.GetType() == typeof(object) ?
+                (new(schema.Fields[i].Schema.Name.ToPascalCase())) :
+                (new(type));
+            _ = compileUnit.AddPropertyWithBackingField(
+                schema.Fields[i].Name,
+                codeTypeReference,
+                schema.Fields[i].Documentation
+            );
+        }
+
+        return compileUnit.Compile();
+    }
+}
---
+++ tests/Devantler.DataMesh.DataProduct.Generator.Tests.Unit/ModelsGeneratorTests/ModelsGeneratorTestsBase.cs
@@ -0,0 +1,23 @@
+using Devantler.DataMesh.DataProduct.Generator.IncrementalGenerators;
+
+namespace Devantler.DataMesh.DataProduct.Generator.Tests.Unit.ModelsGeneratorTests;
+
+public class ModelsGeneratorTestsBase : IncrementalGeneratorTestsBase<ModelsGenerator>
+{
+    protected static CustomAdditionalText CreateAppSettingsWithLocalSchemaRegistryAndSchema(string subject) => new("appsettings.json",
+        $$"""
+        {
+            "DataProduct": {
+                "Schema": {
+                    "Subject": "{{subject}}",
+                    "Version": 1
+                },
+                "SchemaRegistry": {
+                    "Type": "Local",
+                    "Path": "schemas"
+                }
+            }
+        }
+        """
+    );
+}
