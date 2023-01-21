2aa323fe04a50165a709dc3f65ea9b38d31e1105
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-01-22 00:35:52 +0100

    Fixed tests

--- tests/Devantler.DataMesh.DataProduct.Generator.Tests.Unit/ModelsGeneratorTests/GenerateTests.cs
+++ tests/Devantler.DataMesh.DataProduct.Generator.Tests.Unit/ModelsGeneratorTests/GenerateTests.cs
@@ -4,55 +4,13 @@ namespace Devantler.DataMesh.DataProduct.Generator.Tests.Unit.ModelsGeneratorTes
 public class GenerateTests : ModelsGeneratorTestsBase
 {
     [Theory]
-    [InlineData("RecordSchemaEmpty")]
-    [InlineData("RecordSchemaNamespace")]
-    [InlineData("RecordSchemaPrimitiveTypeBoolean")]
-    [InlineData("RecordSchemaPrimitiveTypeBytes")]
-    [InlineData("RecordSchemaPrimitiveTypeDouble")]
-    [InlineData("RecordSchemaPrimitiveTypeFloat")]
-    [InlineData("RecordSchemaPrimitiveTypeInt")]
-    [InlineData("RecordSchemaPrimitiveTypeLong")]
-    [InlineData("RecordSchemaPrimitiveTypeNull")]
-    [InlineData("RecordSchemaPrimitiveTypeString")]
-    [InlineData("RecordSchemaPrimitiveTypes")]
-    public Task GivenValidAppSettingsWithRecordSchema_GenerateValidCode(string subject)
+    [MemberData(nameof(TestCases.ValidCases), MemberType = typeof(TestCases))]
+    public Task GivenValidAppSettings_GeneratesValidCode(string subject)
     {
-        //Arrange
+        //Act
         var additionalText = CreateAppSettingsWithLocalSchemaRegistryAndSchema(subject);
 
-        //Act
         //Assert
-        return Verify(additionalText);
-    }
-
-    [Theory]
-    [InlineData("EnumSchemaEmpty")]
-    [InlineData("EnumSchemaNamespace")]
-    [InlineData("EnumSchemaSymbols")]
-    public Task GivenValidAppSettingsWithEnumSchema_GenerateValidCode(string subject)
-    {
-        //Arrange
-        var additionalText = CreateAppSettingsWithLocalSchemaRegistryAndSchema(subject);
-
-        //Act
-        //Assert
-        return Verify(additionalText);
-    }
-
-    [Theory]
-    [InlineData("UnionSchemaEmpty")]
-    [InlineData("UnionSchemaEnumSchema")]
-    [InlineData("UnionSchemaEnumSchemas")]
-    [InlineData("UnionSchemaMixedSchemas")]
-    [InlineData("UnionSchemaRecordSchema")]
-    [InlineData("UnionSchemaRecordSchemas")]
-    public Task GivenValidAppSettingsWithUnionSchema_GenerateValidCode(string subject)
-    {
-        //Arrange
-        var additionalText = CreateAppSettingsWithLocalSchemaRegistryAndSchema(subject);
-
-        //Act
-        //Assert
-        return Verify(additionalText);
+        return Verify(additionalText).UseMethodName(subject);
     }
 }
---
+++ tests/Devantler.DataMesh.DataProduct.Generator.Tests.Unit/ModelsGeneratorTests/TestCases.cs
@@ -0,0 +1,29 @@
+namespace Devantler.DataMesh.DataProduct.Generator.Tests.Unit.ModelsGeneratorTests;
+
+public static class TestCases
+{
+    public static IEnumerable<object[]> ValidCases =>
+    new List<object[]>
+    {
+        new object[]{"RecordSchemaEmpty" },
+        new object[]{"RecordSchemaNamespace"},
+        new object[]{"RecordSchemaPrimitiveTypeBoolean"},
+        new object[]{"RecordSchemaPrimitiveTypeBytes"},
+        new object[]{"RecordSchemaPrimitiveTypeDouble"},
+        new object[]{"RecordSchemaPrimitiveTypeFloat"},
+        new object[]{"RecordSchemaPrimitiveTypeInt"},
+        new object[]{"RecordSchemaPrimitiveTypeLong"},
+        new object[]{"RecordSchemaPrimitiveTypeNull"},
+        new object[]{"RecordSchemaPrimitiveTypeString"},
+        new object[]{"RecordSchemaPrimitiveTypes"},
+        new object[]{"EnumSchemaEmpty"},
+        new object[]{"EnumSchemaNamespace"},
+        new object[]{"EnumSchemaSymbols"},
+        new object[]{"UnionSchemaEmpty"},
+        new object[]{"UnionSchemaEnumSchema"},
+        new object[]{"UnionSchemaEnumSchemas"},
+        new object[]{"UnionSchemaMixedSchemas"},
+        new object[]{"UnionSchemaRecordSchema"},
+        new object[]{"UnionSchemaRecordSchemas"},
+    };
+}
