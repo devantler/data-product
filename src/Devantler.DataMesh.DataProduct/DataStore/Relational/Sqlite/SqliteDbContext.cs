e827d113ce9a0666be20ef6c76fc1de85f01d1ba
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-02-18 13:34:05 +0100

    Added doc blocks to sqlitecontext

--- src/Devantler.DataMesh.DataProduct/DataStore/Relational/Sqlite/SqliteDbContext.cs
+++ src/Devantler.DataMesh.DataProduct/DataStore/Relational/Sqlite/SqliteDbContext.cs
@@ -3,16 +3,33 @@ using Devantler.DataMesh.DataProduct.DataStore.Relational.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Devantler.DataMesh.DataProduct.DataStore.Relational.Sqlite;
-
+/// <summary>
+/// A Sqlite database context.
+/// </summary>
 public class SqliteDbContext : DbContext
 {
+    /// <summary>
+    /// A constructor to construct a Sqlite database context.
+    /// </summary>
+    /// <param name="options"></param>
     public SqliteDbContext(DbContextOptions<SqliteDbContext> options) : base(options)
     {
     }
 
+    /// <summary>
+    /// A DbSet of Student entities.
+    /// </summary>
     public DbSet<StudentEntity> Students => Set<StudentEntity>();
+
+    /// <summary>
+    /// A DbSet of Enrollment entities.
+    /// </summary>
     public DbSet<EnrollmentEntity> Enrollments => Set<EnrollmentEntity>();
 
+    /// <summary>
+    /// Configures the model used by the context.
+    /// </summary>
+    /// <param name="modelBuilder"></param>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         _ = modelBuilder.Entity<StudentEntity>().ToTable("Student");
