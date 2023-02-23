9bafc259f96b1028b9c45ea8c24ebc4bca5bba49
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-02-23 08:45:46 +0100

    Added unit tests

---
+++ tests/Devantler.DataMesh.DataProduct.Generator.Tests.Unit/RestApiControllerGeneratorTests/GenerateTests.cs
@@ -0,0 +1,37 @@
+using Devantler.DataMesh.DataProduct.Generator.IncrementalGenerators;
+
+namespace Devantler.DataMesh.DataProduct.Generator.Tests.Unit.RestApiControllerGeneratorTests;
+
+[UsesVerify]
+public class GenerateTests : IncrementalGeneratorTestsBase<RestApiControllerGenerator>
+{
+    [Theory]
+    [MemberData(nameof(TestCases.ValidCases), MemberType = typeof(TestCases))]
+    public Task GivenValidAppSettings_GeneratesValidCode(string subject)
+    {
+        //Arrange
+        var additionalText = CreateAppSettings(
+            /*lang=json,strict*/
+            $$"""
+            {
+                "DataProduct": {
+                    "Schema": {
+                        "Subject": "{{subject}}",
+                        "Version": 1
+                    },
+                    "SchemaRegistry": {
+                        "Type": "Local",
+                        "Path": "Schemas"
+                    }
+                }
+            }
+            """
+        );
+
+        //Act
+        var driver = RunGenerator(additionalText);
+
+        //Assert
+        return Verify(driver).UseMethodName(subject).DisableRequireUniquePrefix();
+    }
+}
