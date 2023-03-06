e3859d07fa638d0d4b9012ff4dbc2c0cf75b870b
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-03-06 14:29:13 +0100

    Test

---
+++ tests/Devantler.DataMesh.DataProduct.Generator.Tests.Unit/IncrementalGenerators/GraphQlQueryGeneratorTests/GenerateTests.cs
@@ -0,0 +1,38 @@
+using Devantler.DataMesh.DataProduct.Generator.IncrementalGenerators;
+
+namespace Devantler.DataMesh.DataProduct.Generator.Tests.Unit.IncrementalGenerators.GraphQlQueryGeneratorTests;
+
+[UsesVerify]
+public class GenerateTests : IncrementalGeneratorTestsBase<GraphQLQueryGenerator>
+{
+    [Theory]
+    [MemberData(nameof(TestCases.ValidCases), MemberType = typeof(TestCases))]
+    public Task GivenValidAppSettings_GeneratesValidCode(string subject)
+    {
+        //Arrange
+        var additionalText = CreateAppSettings(
+            $$"""
+            {
+                "DataProduct": {
+                    "Services": {
+                        "SchemaRegistry": {
+                            "Type": "Local",
+                            "Path": "schemas",
+                            "Schema": {
+                                "Subject": "{{subject}}",
+                                "Version": 1
+                            }
+                        }
+                    }
+                }
+            }
+            """
+        );
+
+        //Act
+        var driver = RunGenerator(additionalText);
+
+        //Assert
+        return Verify(driver).UseMethodName(subject).DisableRequireUniquePrefix();
+    }
+}
