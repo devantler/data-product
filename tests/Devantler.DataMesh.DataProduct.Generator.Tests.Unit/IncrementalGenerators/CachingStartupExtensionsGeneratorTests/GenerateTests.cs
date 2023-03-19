f10db1dc4b28dae2dd3246967f789652e169f5b8
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-03-19 22:27:26 +0100

    Added CachingStartupExtensionsGenerator tests

---
+++ tests/Devantler.DataMesh.DataProduct.Generator.Tests.Unit/IncrementalGenerators/CachingStartupExtensionsGeneratorTests/GenerateTests.cs
@@ -0,0 +1,40 @@
+using Devantler.DataMesh.DataProduct.Generator.IncrementalGenerators;
+
+namespace Devantler.DataMesh.DataProduct.Generator.Tests.Unit.IncrementalGenerators.CachingStartupExtensionsGeneratorTests;
+
+[UsesVerify]
+public class GenerateTests : IncrementalGeneratorTestsBase<CachingStartupExtensionsGenerator>
+{
+    [Theory]
+    [MemberData(nameof(TestCases.ValidCases), MemberType = typeof(TestCases))]
+    public Task GivenValidDataProductConfig_GeneratesValidCode(string subject)
+    {
+        //Arrange
+        var additionalText = CreateDataProductConfig(
+            $$"""
+            {
+                "FeatureFlags": {
+                    "EnableCaching": true
+                },
+                "SchemaRegistry": {
+                    "Type": "Local",
+                    "Path": "schemas",
+                    "Schema": {
+                        "Subject": "{{subject}}",
+                        "Version": 1
+                    }
+                },
+                "CacheStore": {
+                    "Type": "InMemory"
+                }
+            }
+            """
+        );
+
+        //Act
+        var driver = RunGenerator(additionalText);
+
+        //Assert
+        return Verify(driver).UseMethodName(subject).DisableRequireUniquePrefix();
+    }
+}
