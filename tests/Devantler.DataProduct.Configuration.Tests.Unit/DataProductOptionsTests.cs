7e582d4510edb72d98b4c5b3b64ed1fcd1987a96
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-05-26 00:38:38 +0200

    Added unit tests to test default values of data product options (#219)

---
+++ tests/Devantler.DataProduct.Configuration.Tests.Unit/DataProductOptionsTests.cs
@@ -0,0 +1,17 @@
+using Devantler.DataProduct.Configuration.Options;
+
+namespace Devantler.DataProduct.Configuration.Tests.Unit;
+
+[UsesVerify]
+public class DataProductOptionsTests
+{
+    [Fact]
+    public Task DefaultPropertyValues_ShouldBeInitialized()
+    {
+        //Arrange
+        var options = new DataProductOptions();
+
+        //Act
+        return Verify(options);
+    }
+}
