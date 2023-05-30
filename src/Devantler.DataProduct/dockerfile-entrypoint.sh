0983f15f0ec0e29ef78cd106ef22f57d44dfc14f
Nikolai Emil Damm <nidam16@student.sdu.dk>
2023-05-30 16:21:48 +0200

    Changed .sh environment variable to use ${...} instead of $..., as it is generally safer

--- src/Devantler.DataProduct/dockerfile-entrypoint.sh
+++ src/Devantler.DataProduct/dockerfile-entrypoint.sh
@@ -6,7 +6,7 @@ fi
 
 echo "📦 Publishing Data Product 📦"
 
-dotnet publish /build/src/Devantler.DataProduct -c Release --no-restore -o /app -a $TARGETARCHENV
+dotnet publish /build/src/Devantler.DataProduct -c Release --no-restore -o /app -a ${TARGETARCHENV}
 
 echo "🧹 Cleaning up 🧹"
 apt-get autoremove -y
