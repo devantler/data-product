ac2a194a9fc9fab909fa1a4ea60ff662ae35500f
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-11-04 22:43:27 +0100

    fix: Remove default value for cancellationToken in GetRecordSchemaPrimitiveTypes method

--- tests/Devantler.DataProduct.Generator.Tests.Unit/IncrementalGenerators/GraphQlQueryGeneratorTests/GenerateTests.record-schema-primitive-types#Query.g.verified.cs
+++ tests/Devantler.DataProduct.Generator.Tests.Unit/IncrementalGenerators/GraphQlQueryGeneratorTests/GenerateTests.record-schema-primitive-types#Query.g.verified.cs
@@ -14,7 +14,7 @@ public partial class Query
     [UseProjection]
     [UseFiltering]
     [UseSorting]
-  public async Task<IEnumerable<RecordSchemaPrimitiveTypes>> GetRecordSchemaPrimitiveTypes([Service] IDataStoreService<Guid, RecordSchemaPrimitiveTypes> dataStoreService, CancellationToken cancellationToken = default)
+    public async Task<IEnumerable<RecordSchemaPrimitiveTypes>> GetRecordSchemaPrimitiveTypes([Service] IDataStoreService<Guid, RecordSchemaPrimitiveTypes> dataStoreService, CancellationToken cancellationToken)
         => await dataStoreService.ReadAllAsync(cancellationToken);
 
 }
